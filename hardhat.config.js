6c026036b5e638b962213e9205a1a79eea304878
Roberto Leon-Luna <rsleonluna>
2022-09-14 20:35:13 -0400

    adding interfaces and borrwoing

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,12 +13,16 @@ const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
 
 module.exports = {
     defaultNetwork: "hardhat",
     networks: {
         hardhat: {
             chainId: 31337,
+            forking: {
+                url: MAINNET_RPC_URL,
+            },
             blockConfirmations: 1,
         },
         localhost: {
@@ -37,7 +41,14 @@ module.exports = {
             accounts: [PRIVATE_KEY],
         },
     },
-    solidity: "0.8.7",
+    solidity: {
+        compilers: [
+            { version: "0.4.19" },
+            { version: "0.8.7" },
+            { version: "0.6.6" },
+            { version: "0.6.12" },
+        ],
+    },
     gasReporter: {
         enabled: false,
         outputFile: "gas-reporter.txt",
--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,4 +1,45 @@
-async function main() {}
+const { getNamedAccounts, ethers } = require("hardhat")
+const { getWeth, AMOUNT } = require("../scripts/getWeth")
+
+async function main() {
+    //the protocol treats everything like an ERC 20 token
+    await getWeth()
+    const { deployer } = await getNamedAccounts()
+
+    //Lending pool address provider: 0xb53c1a33016b2dc2ff3653530bff1848a515c8c5
+    //Lending pool ^
+
+    const lendingPool = await getLendingPool(deployer)
+    console.log(`LendingPool address ${lendingPool.address}`)
+
+    //deposit
+    const wethTokenAddress = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
+
+    //approve
+
+    await approveErc20(wethTokenAddress, lendingPool.address, AMOUNT, deployer)
+    console.log("Depositing ...")
+    await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0)
+    console.log("Deposited")
+}
+
+async function approveErc20(erc20Address, spenderAddress, amountToSpend, account) {
+    const erc20Token = await ethers.getContractAt("IERC20", erc20Address, account)
+    const tx = await erc20Token.approve(spenderAddress, amountToSpend)
+    await tx.wait(1)
+    console.log("Approved!")
+}
+
+async function getLendingPool(account) {
+    const lendingPoolAddressesProvider = await ethers.getContractAt(
+        "ILendingPoolAddressesProvider",
+        "0xb53c1a33016b2dc2ff3653530bff1848a515c8c5",
+        account
+    )
+    const lendingPoolAddress = await lendingPoolAddressesProvider.getLendingPool()
+    const lendingPool = await ethers.getContractAt("ILendingPool", lendingPoolAddress, account)
+    return lendingPool
+}
 
 main()
     .then(() => process.exit(0))
